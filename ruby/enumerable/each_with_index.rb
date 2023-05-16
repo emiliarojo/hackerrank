4cecb86429fb415526a66541c7baaddb2d6dea29
Emilia Rojo <emiliamrojo@gmail.com>
2023-05-16 11:59:47 +0200

    Ruby enumerable

--- ruby/enumerable/each_with_index.rb
+++ ruby/enumerable/each_with_index.rb
@@ -1,3 +1,9 @@
+=begin
+Implement skip_animals method that takes an animals array
+and a skip integer and returns an array of all elements
+except first skip number of items
+=end
+
 def skip_animals(animals, skip)
   array = []
   animals.each_with_index do |animal, index|
